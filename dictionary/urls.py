7cac2b092e7cee87dc6d7756a7340b92e693a5b8
Sandeep <skumar8work@gmail.com>
2022-06-16 19:20:21 +0530

    first commit

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -2,5 +2,5 @@ from django.urls import  path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('word/',views.word,name='word'),
+    path('word',views.word,name='word'),
 ]
